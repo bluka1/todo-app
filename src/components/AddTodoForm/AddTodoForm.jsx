bc62fabbc8f7776cb4934f6dcf055224d7a486d9
bluka1 <luka.batarelo@gmail.com>
2023-12-21 20:59:04 +0100

    first commit

---
+++ src/components/AddTodoForm/AddTodoForm.jsx
@@ -0,0 +1,33 @@
+import { useState } from 'react';
+import PropTypes from 'prop-types';
+import styles from './AddTodoForm.module.css';
+
+const AddTodoForm = (props) => {
+	const [text, setText] = useState('');
+
+	const handleFormSubmit = (e) => {
+		e.preventDefault();
+		console.log('TODO TEXT:', text);
+		setText('');
+	};
+	return (
+		<form className={styles['add-todo-form']} onSubmit={handleFormSubmit}>
+			<input
+				className={styles['add-todo-form__input']}
+				type='text'
+				name='text'
+				id='text'
+				placeholder='Add todo'
+				value={text}
+				onChange={(e) => setText(e.target.value)}
+			/>
+			<button className={styles['add-todo-form__button']} type='submit'>
+				ADD
+			</button>
+		</form>
+	);
+};
+
+AddTodoForm.propTypes = {};
+
+export default AddTodoForm;
