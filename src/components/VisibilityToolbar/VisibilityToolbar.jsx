734ec2c5b26025630b6ade2db9d0bcd8d997a75c
bluka1 <luka.batarelo@gmail.com>
2024-01-03 20:59:21 +0100

    add toggle button styles

--- src/components/VisibilityToolbar/VisibilityToolbar.jsx
+++ src/components/VisibilityToolbar/VisibilityToolbar.jsx
@@ -1,19 +1,30 @@
 import PropTypes from 'prop-types';
 import { ToggleButtonGroup, ToggleButton } from 'react-bootstrap';
 import { VISIBILITY_TYPES } from '../../utils/visibilityTypes';
+import styles from './VisibilityToolbar.module.css';
 
 const { ALL, ACTIVE, COMPLETED } = VISIBILITY_TYPES;
 
 const VisibilityToolbar = (props) => {
 	return (
-		<ToggleButtonGroup type='radio' name='visibilityOptions' defaultValue={ALL}>
-			<ToggleButton value={ALL} size='sm' className='btn'>
+		<ToggleButtonGroup
+			type='radio'
+			name='visibilityOptions'
+			defaultValue={ALL}
+			className={styles['toggle-button-group']}
+		>
+			<ToggleButton
+				value={ALL}
+				size='sm'
+				className={'toggle-button'}
+				onClick={() => {}}
+			>
 				All
 			</ToggleButton>
-			<ToggleButton value={ACTIVE} size='sm' className='btn'>
+			<ToggleButton value={ACTIVE} size='sm' className={'toggle-button'}>
 				Active
 			</ToggleButton>
-			<ToggleButton value={COMPLETED} size='sm' className='btn'>
+			<ToggleButton value={COMPLETED} size='sm' className={'toggle-button'}>
 				Completed
 			</ToggleButton>
 		</ToggleButtonGroup>
